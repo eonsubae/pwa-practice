c36100f0d3c1475ba6da351ceb236c69e67719f1
eonsubae <eonsubae@naver.com>
2020-05-16 07:03:43 +0900

    Serviceworker registration

---
+++ serviceworker-registration/sw.js
@@ -0,0 +1,12 @@
+
+// Service Worker
+self.addEventListener('install', (e) => {
+
+  e.waitUntil(new Promise((resolve) => {
+    setTimeout(resolve, 5000);
+  }))
+});
+
+self.addEventListener('activate', () => {
+  console.log('SW2 Active');
+});
